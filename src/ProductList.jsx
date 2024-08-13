5b5181bf33b20e5c180111b4bace527848a5337f
Aaila Sohail <atiba.sohail75@gmail.com>
2024-08-13 17:35:15 +0500

    Adding Product to the cart

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
 import "./ProductList.css";
+import { useDispatch } from 'react-redux';
 import CartItem from "./CartItem";
+import { addItem } from  "./CartSlice"
+
+
 function ProductList() {
   const [showCart, setShowCart] = useState(false);
   const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-
+  const [addedToCart, setAddedToCart] = useState({});
+  const dispatch = useDispatch();
   const plantsArray = [
     {
       category: "Air Purifying Plants",
@@ -280,6 +285,15 @@ function ProductList() {
   const handleContinueShopping = (e) => {
     e.preventDefault();
     setShowCart(false);
+  };
+
+  const handleAddToCart = (product) => {
+    dispatch(addItem(product));
+    setAddedToCart((prevState) => ({
+      ...prevState,
+      [product.name]: true,
+    }));
+
   };
   return (
     <div>

a45c74d5e8ebd21c75f134f8882d90c89b95d132
Aaila Sohail <atiba.sohail75@gmail.com>
2024-08-13 23:46:27 +0500

    final

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./ProductList.css";
-import { useDispatch,useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import CartItem from "./CartItem";
-import { addItem } from  "./CartSlice"
-
+import { addItem } from "./CartSlice";
 
 function ProductList() {
   const [showCart, setShowCart] = useState(false);
   const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
   const [addedToCart, setAddedToCart] = useState({});
-  const cartItems = useSelector(state => state.cart.items);
+  const cartItems = useSelector((state) => state.cart.items);
   const dispatch = useDispatch();
   const plantsArray = [
     {
@@ -293,7 +292,14 @@ function ProductList() {
       ...prevState,
       [product.name]: true,
     }));
+  };
 
+  const calculateTotalItems = () => {
+    const totalItemsInCart = cartItems.reduce(
+      (total, item) => total + item.quantity,
+      0
+    );
+    return totalItemsInCart;
   };
   return (
     <div>
@@ -323,26 +329,29 @@ function ProductList() {
             {" "}
             <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
               <h1 className="cart">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 256 256"
-                  id="IconChangeColor"
-                  height="68"
-                  width="68"
-                >
-                  <rect width="156" height="156" fill="none"></rect>
-                  <circle cx="80" cy="216" r="12"></circle>
-                  <circle cx="184" cy="216" r="12"></circle>
-                  <path
-                    d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8"
-                    fill="none"
-                    stroke="#faf9f9"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    id="mainIconPathAttribute"
-                  ></path>
-                </svg>
+                <div className="cart-container">
+                  <div className="total-items">{calculateTotalItems()}</div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 256 256"
+                    id="IconChangeColor"
+                    height="68"
+                    width="68"
+                  >
+                    <rect width="156" height="156" fill="none"></rect>
+                    <circle cx="80" cy="216" r="12"></circle>
+                    <circle cx="184" cy="216" r="12"></circle>
+                    <path
+                      d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8"
+                      fill="none"
+                      stroke="#faf9f9"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      id="mainIconPathAttribute"
+                    ></path>
+                  </svg>
+                </div>
               </h1>
             </a>
           </div>
@@ -357,7 +366,9 @@ function ProductList() {
               </h1>
               <div className="product-list">
                 {category.plants.map((plant, plantIndex) => {
-                  const isAdded = cartItems.some(item => item.name === plant.name);
+                  const isAdded = cartItems.some(
+                    (item) => item.name === plant.name
+                  );
                   return (
                     <div className="product-card" key={plantIndex}>
                       <img
@@ -370,9 +381,17 @@ function ProductList() {
                       <div className="product-des">{plant.description}</div>
                       <button
                         className="product-button"
-                        onClick={() => isAdded ? handleRemoveFromCart(plant) : handleAddToCart(plant)}
+                        onClick={() =>
+                          isAdded
+                            ? handleRemoveFromCart(plant)
+                            : handleAddToCart(plant)
+                        }
                         disabled={isAdded}
-                        style={isAdded ? { backgroundColor: "gray", cursor: "not-allowed" } : {}}
+                        style={
+                          isAdded
+                            ? { backgroundColor: "gray", cursor: "not-allowed" }
+                            : {}
+                        }
                       >
                         {isAdded ? "Added to Cart" : "Add to Cart"}
                       </button>
